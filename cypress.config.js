264b8edac55f00fc00addc59a42a4852df8256a9
Marcelo <marcelojxt@gmail.com>
2025-11-19 14:29:42 -0300

    feat: Adicionando testes multi ambientes

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.BASE_URL,
     viewportWidth: 1920,
     viewportHeight: 1080,
     screenshotOnRunFailure: false,
